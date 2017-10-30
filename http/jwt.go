b538e1e112f43a065b642727d30c93af2be006ac
Aditya Bhasin <conlini@gmail.com>
2017-10-30 20:03:53 +0530

    added jwt-go library and removed old vendor files

--- http/jwt.go
+++ http/jwt.go
@@ -1,9 +1,14 @@
 package http
 
+import (
+	_ "github.com/dgrijalva/jwt-go"
+)
+
 // we need functions that do the following
 //
 // 1. generate a new jwt token and stick user claims
 //    user claims can be - user id and device type for now
 //    for now - expireAt will be 1 hours -- default and hard coded
+//    we will also stick the genereated token in the header of the request and redirect to desired site
 // 2. authenticate a jwt on a http reqeust
 // 3. possibly we want to re-validate a jwt with expire at extended
